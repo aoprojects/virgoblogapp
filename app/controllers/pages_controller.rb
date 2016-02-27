5138d83af803d4469b03acde054e4b5cb7e5f8a1
Alison Ostendorf <aostendorf@gmail.com>
2016-02-27 17:27:04 -0500

    added render layout: 'application' to pages controller actions so that css would connect

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,20 @@
 class PagesController < ActionController::Base
 
+
 	def blog
+		render layout: 'application'
 	end
 
 	def about
+		render layout: 'application'
 	end
 
 	def ventures
+		render layout: 'application'
 	end
 
 	def properties
+		render layout: 'application'
 	end
 
 end
