1edc4bfa71f247a5e9e57bb4a92e3a2e4ea46a8c
Alison Ostendorf <aostendorf@gmail.com>
2016-02-27 12:12:41 -0500

    added all 4 pages and routes to app

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+class PagesController < ActionController::Base
+
+	def blog
+	end
+
+	def about
+	end
+
+	def ventures
+	end
+
+	def properties
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  root 'pages#blog'
+  get 'about' => 'pages#about'
+  get 'properties' => 'pages#properties'
+  get 'ventures' => 'pages#ventures'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
