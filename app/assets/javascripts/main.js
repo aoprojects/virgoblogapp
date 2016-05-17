38ca8367b823eebd2475d316c91cd36508c28ab7
Alison Ostendorf <aostendorf@gmail.com>
2016-05-17 14:56:25 -0400

    articles summary is now showing

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,10 +2,4 @@ $( document ).ready(function() {
 	$( "#xs_hamburger" ).click(function() {
 		$( "#xs_nav_nojs" ).toggle( "fast" );
 	});
-
-	$( "#articles_summary" ).click(function() {
-		$( "#articles_list" ).toggle( "fast" );
-		$( ".start_hidden" ).toggle( "fast" );
-		$( ".start_visible" ).toggle( "fast" );
-	});
 });
