c73f257ca6f93ba5400baec48ea5ad7e4e3a0c40
Alison Ostendorf <aostendorf@gmail.com>
2016-07-06 15:54:45 -0400

    implement toggle of rate category click to display details

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,4 +2,9 @@ $( document ).ready(function() {
 	$( "#xs_hamburger" ).click(function() {
 		$( "#xs_nav_nojs" ).toggle( "fast" );
 	});
+
+	$( ".rate_category").click(function() {
+		$(this).next(".rate_category_details").toggle("fast");
+	});
 });
+
